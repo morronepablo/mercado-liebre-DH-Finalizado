b2527b737dfb7b556ceea152a4db5c3fa668e528
morronepablo <morronepablo@gmail.com>
2023-02-08 00:35:57 -0300

    ajustes

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.get('/users', (req, res) => {
     res.send(users);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Levantando un servidor con Express en ", "http://localhost:3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Levantando un servidor con Express en: http://localhost:${port}`);
 });
